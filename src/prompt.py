07b93d1ee467d83dcd8de58323a5f4e7e7c8cec1
andyperrine <andyperrine@gmail.com>
2024-11-18 07:53:34 +0000

    Second assignment, AndyP

--- src/prompt.py
+++ src/prompt.py
@@ -3,5 +3,6 @@ Generate a valid SQL query for the following natural language instruction:
 
 Query: ${query}
 
+Only generate SQL code and nothing else. name the columns.  Do not include formatting or string escaping.
 ${gr.complete_json_suffix_v3}
 """
